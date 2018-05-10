c89daaa4630cac2bc982ff1f6ea04e6c7ade1d61
Tsutomu Kawamura <cognitom@gmail.com>
2018-05-10 15:08:09 +0000

    Adds zen-observable-adapter to fix the issue on zen-observable

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,15 @@
-import uglify from "rollup-plugin-uglify"
+import { join } from "path"
+import alias from "rollup-plugin-alias"
 import resolve from "rollup-plugin-node-resolve"
+import uglify from "rollup-plugin-uglify"
 
 const production = !process.env.ROLLUP_WATCH
 
 export default {
   plugins: [
+    alias({
+      "zen-observable": join(__dirname, "zen-observable-adapter.js")
+    }),
     resolve(),
     production && uglify()
   ],
---
+++ zen-observable-adapter.js
@@ -0,0 +1,7 @@
+/**
+ * This adapter is needed as a temporal solution
+ * for the issue on zen-observable.
+ * https://github.com/zenparsing/zen-observable/issues/31
+ * https://github.com/zenparsing/zen-observable/pull/33
+ */
+export { Observable as default } from "./node_modules/zen-observable/src/Observable.js"
